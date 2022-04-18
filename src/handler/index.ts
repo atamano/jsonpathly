ac77bf5269b8b744347e3f71eaba282f9a758f4a
atamano <antoine.tamano@gmail.com>
2022-04-18 23:08:59 +0200

    feat: allow root in filters scripts

--- src/handler/index.ts
+++ src/handler/index.ts
@@ -27,278 +27,289 @@ const isObject = (item: unknown): item is Record<string, unknown> => {
   return isPlainObject(item);
 };
 
-const handleIdentifier = (payload: unknown, tree: Identifier): unknown => {
-  if (!isObject(payload) && !isArray(payload)) {
-    return;
+class Handler {
+  rootPayload: unknown;
+
+  constructor(rootPayload: unknown) {
+    this.rootPayload = rootPayload;
   }
 
-  if (tree.value === WILDCARD) {
-    return Object.values(payload);
-  }
+  handleIdentifier = (payload: unknown, tree: Identifier): unknown => {
+    if (!isObject(payload) && !isArray(payload)) {
+      return;
+    }
 
-  if (isArray(payload) || !isObject(payload) || !(tree.value in payload)) {
-    return;
-  }
+    if (tree.value === WILDCARD) {
+      return Object.values(payload);
+    }
 
-  return payload[tree.value];
-};
+    if (isArray(payload) || !isObject(payload) || !(tree.value in payload)) {
+      return;
+    }
 
-const handleComparatorArgument = (payload: unknown, tree: ComparatorArgument): unknown => {
-  switch (tree.type) {
-    case 'root': {
-      // TODO
-      return handleSubscript(payload, tree.next);
-    }
-    case 'current': {
-      return handleSubscript(payload, tree.next);
-    }
-    case 'value': {
-      return tree.value;
-    }
-  }
-};
-
-const handleComparator = (payload: unknown, tree: Comparator): boolean => {
-  const leftValue = handleComparatorArgument(payload, tree.left);
-  const rightValue = handleComparatorArgument(payload, tree.right);
-
-  switch (tree.operator) {
-    case 'eq': {
-      return leftValue === rightValue;
-    }
-    case 'ne': {
-      return leftValue !== rightValue;
-    }
-    case 'lt': {
-      return leftValue < rightValue;
-    }
-    case 'le': {
-      return leftValue <= rightValue;
-    }
-    case 'gt': {
-      return leftValue > rightValue;
-    }
-    case 'ge': {
-      return leftValue >= rightValue;
-    }
-    case 'in': {
-      if (isArray(rightValue)) {
-        return rightValue.includes(leftValue);
-      }
-      return false;
-    }
-    case 'nin': {
-      if (isArray(rightValue)) {
-        return !rightValue.includes(leftValue);
-      }
-      return false;
-    }
-  }
-};
-
-const handleBinaryExpression = (payload: unknown, tree: BinaryExpression): boolean => {
-  const leftValue = handleFilterExpressionChild(payload, tree.left);
-  const rightValue = handleFilterExpressionChild(payload, tree.right);
-  switch (tree.operator) {
-    case 'and': {
-      return leftValue && rightValue;
-    }
-    case 'or': {
-      return leftValue || rightValue;
-    }
-  }
-};
-
-const handleFilterExpressionChild = (payload: unknown, tree: FilterExpressionChild): boolean => {
-  switch (tree.type) {
-    case 'binary_expression': {
-      return handleBinaryExpression(payload, tree);
-    }
-    case 'comparator': {
-      return handleComparator(payload, tree);
-    }
-    case 'group_expression': {
-      return handleFilterExpressionChild(payload, tree.value);
-    }
-    case 'negate': {
-      return !handleFilterExpressionChild(payload, tree.value);
-    }
-    case 'root': {
-      // TODO
-      return !!handleSubscript(payload, tree.next);
-    }
-    case 'current': {
-      return !!handleSubscript(payload, tree.next);
-    }
-  }
-};
-
-const handleNumericLiteral = (payload: unknown, tree: NumericLiteral): unknown => {
-  if (isArray(payload) && tree.value < payload.length) {
     return payload[tree.value];
-  }
+  };
 
-  if (typeof payload === 'string' && tree.value < payload.length) {
+  handleComparatorArgument = (payload: unknown, tree: ComparatorArgument): unknown => {
+    switch (tree.type) {
+      case 'root': {
+        return this.handleSubscript(this.rootPayload, tree.next);
+      }
+      case 'current': {
+        return this.handleSubscript(payload, tree.next);
+      }
+      case 'value': {
+        return tree.value;
+      }
+    }
+  };
+
+  handleComparator = (payload: unknown, tree: Comparator): boolean => {
+    const leftValue = this.handleComparatorArgument(payload, tree.left);
+    const rightValue = this.handleComparatorArgument(payload, tree.right);
+
+    switch (tree.operator) {
+      case 'eq': {
+        return leftValue === rightValue;
+      }
+      case 'ne': {
+        return leftValue !== rightValue;
+      }
+      case 'lt': {
+        return leftValue < rightValue;
+      }
+      case 'le': {
+        return leftValue <= rightValue;
+      }
+      case 'gt': {
+        return leftValue > rightValue;
+      }
+      case 'ge': {
+        return leftValue >= rightValue;
+      }
+      case 'in': {
+        if (isArray(rightValue)) {
+          return rightValue.includes(leftValue);
+        }
+        return false;
+      }
+      case 'nin': {
+        if (isArray(rightValue)) {
+          return !rightValue.includes(leftValue);
+        }
+        return false;
+      }
+    }
+  };
+
+  handleBinaryExpression = (payload: unknown, tree: BinaryExpression): boolean => {
+    const leftValue = this.handleFilterExpressionChild(payload, tree.left);
+    const rightValue = this.handleFilterExpressionChild(payload, tree.right);
+    switch (tree.operator) {
+      case 'and': {
+        return leftValue && rightValue;
+      }
+      case 'or': {
+        return leftValue || rightValue;
+      }
+    }
+  };
+
+  handleFilterExpressionChild = (payload: unknown, tree: FilterExpressionChild): boolean => {
+    switch (tree.type) {
+      case 'binary_expression': {
+        return this.handleBinaryExpression(payload, tree);
+      }
+      case 'comparator': {
+        return this.handleComparator(payload, tree);
+      }
+      case 'group_expression': {
+        return this.handleFilterExpressionChild(payload, tree.value);
+      }
+      case 'negate': {
+        return !this.handleFilterExpressionChild(payload, tree.value);
+      }
+      case 'root': {
+        return !!this.handleSubscript(this.rootPayload, tree.next);
+      }
+      case 'current': {
+        return !!this.handleSubscript(payload, tree.next);
+      }
+    }
+  };
+
+  handleNumericLiteral = (payload: unknown, tree: NumericLiteral): unknown => {
+    if (isArray(payload) && tree.value < payload.length) {
+      return payload[tree.value];
+    }
+
+    if (typeof payload === 'string' && tree.value < payload.length) {
+      return payload[tree.value];
+    }
+  };
+
+  handleArraySlice = (payload: unknown, tree: ArraySlice): unknown[] => {
+    if (!isArray(payload)) {
+      return;
+    }
+
+    const results = [];
+    const result = payload.slice(
+      tree.start !== null ? tree.start : undefined,
+      tree.end !== null ? tree.end : undefined,
+    );
+    const step = tree.step === null || tree.step <= 0 ? 1 : tree.step;
+
+    for (let i = 0; i < result.length; i++) {
+      if (i % step === 0) {
+        results.push(result[i]);
+      }
+    }
+
+    return results;
+  };
+
+  handleStringLiteral = (payload: unknown, tree: StringLiteral): unknown => {
+    if (!isObject(payload) || !(tree.value in payload)) {
+      return;
+    }
+
     return payload[tree.value];
-  }
-};
+  };
 
-const handleArraySlice = (payload: unknown, tree: ArraySlice): unknown[] => {
-  if (!isArray(payload)) {
-    return;
-  }
+  handleScriptExpression = (payload: unknown, tree: ScriptExpression): unknown => {
+    console.log('Script expression', tree);
 
-  const results = [];
-  const result = payload.slice(tree.start !== null ? tree.start : undefined, tree.end !== null ? tree.end : undefined);
-  const step = tree.step === null || tree.step <= 0 ? 1 : tree.step;
+    return payload;
+  };
 
-  for (let i = 0; i < result.length; i++) {
-    if (i % step === 0) {
-      results.push(result[i]);
-    }
-  }
-
-  return results;
-};
-
-const handleStringLiteral = (payload: unknown, tree: StringLiteral): unknown => {
-  if (!isObject(payload) || !(tree.value in payload)) {
-    return;
-  }
-
-  return payload[tree.value];
-};
-
-const handleScriptExpression = (payload: unknown, tree: ScriptExpression): unknown => {
-  console.log('Script expression', tree);
-
-  return payload;
-};
-
-const handleSubscriptable = (payload: unknown, tree: Subscriptable): unknown => {
-  switch (tree.type) {
-    case 'identifier': {
-      return handleIdentifier(payload, tree);
-    }
-    case 'string_literal': {
-      return handleStringLiteral(payload, tree);
-    }
-    case 'numeric_literal': {
-      return handleNumericLiteral(payload, tree);
-    }
-    case 'filter_expression': {
-      let results = [];
-      if (isArray(payload)) {
-        for (const item of payload) {
-          const isValid = handleFilterExpressionChild(item, tree.value);
-          if (isValid) {
-            results = results.concat(item);
+  handleSubscriptable = (payload: unknown, tree: Subscriptable): unknown => {
+    switch (tree.type) {
+      case 'identifier': {
+        return this.handleIdentifier(payload, tree);
+      }
+      case 'string_literal': {
+        return this.handleStringLiteral(payload, tree);
+      }
+      case 'numeric_literal': {
+        return this.handleNumericLiteral(payload, tree);
+      }
+      case 'filter_expression': {
+        let results = [];
+        if (isArray(payload)) {
+          for (const item of payload) {
+            const isValid = this.handleFilterExpressionChild(item, tree.value);
+            if (isValid) {
+              results = results.concat(item);
+            }
           }
         }
+        // Handle objects?
+        return results;
       }
-      // Handle objects?
-      return results;
+      case 'array_slice': {
+        return this.handleArraySlice(payload, tree);
+      }
+      case 'script_expression': {
+        return this.handleScriptExpression(payload, tree);
+      }
     }
-    case 'array_slice': {
-      return handleArraySlice(payload, tree);
-    }
-    case 'script_expression': {
-      return handleScriptExpression(payload, tree);
-    }
-  }
-};
+  };
 
-const handleSubscriptables = (payload: unknown, tree: Subscriptables): unknown[] => {
-  let results = [];
+  handleSubscriptables = (payload: unknown, tree: Subscriptables): unknown[] => {
+    let results = [];
 
-  for (const treeValue of tree.values) {
-    const res = handleSubscriptable(payload, treeValue);
-    if (typeof res !== 'undefined') {
-      results = results.concat(res);
-    }
-  }
-
-  return results;
-};
-
-const handleSubscriptBracket = (payload: unknown, tree: SubscriptBracket): unknown => {
-  const results = handleSubscriptables(payload, tree.value);
-
-  if (
-    tree.value.values.length === 1 &&
-    ['string_literal', 'numeric_literal', 'identifier'].includes(tree.value.values[0].type)
-  ) {
-    return handleSubscript(results.pop(), tree.next);
-  }
-
-  return handleSubscript(results, tree.next);
-};
-
-const handleSubscriptDotdot = (payload: unknown, tree: SubscriptDotdot): unknown => {
-  const treeValue = tree.value;
-
-  switch (treeValue.type) {
-    case 'subscriptables': {
-      return handleSubscriptables(payload, treeValue);
-    }
-    case 'identifier': {
-      let results = [];
-      const identifierRes = handleIdentifier(payload, treeValue);
-      const res = handleSubscript(identifierRes, tree.next);
+    for (const treeValue of tree.values) {
+      const res = this.handleSubscriptable(payload, treeValue);
       if (typeof res !== 'undefined') {
         results = results.concat(res);
       }
+    }
 
-      if (isObject(payload)) {
-        for (const value of Object.values(payload)) {
-          const res = handleSubscriptDotdot(value, tree);
+    return results;
+  };
+
+  handleSubscriptBracket = (payload: unknown, tree: SubscriptBracket): unknown => {
+    const results = this.handleSubscriptables(payload, tree.value);
+
+    if (
+      tree.value.values.length === 1 &&
+      ['string_literal', 'numeric_literal', 'identifier'].includes(tree.value.values[0].type)
+    ) {
+      return this.handleSubscript(results.pop(), tree.next);
+    }
+
+    return this.handleSubscript(results, tree.next);
+  };
+
+  handleSubscriptDotdot = (payload: unknown, tree: SubscriptDotdot): unknown => {
+    const treeValue = tree.value;
+
+    switch (treeValue.type) {
+      case 'subscriptables': {
+        return this.handleSubscriptables(payload, treeValue);
+      }
+      case 'identifier': {
+        let results = [];
+        const identifierRes = this.handleIdentifier(payload, treeValue);
+        const res = this.handleSubscript(identifierRes, tree.next);
+        if (typeof res !== 'undefined') {
           results = results.concat(res);
         }
-      }
-      if (isArray(payload)) {
-        for (const item of payload) {
-          const res = handleSubscriptDotdot(item, tree);
-          results = results.concat(res);
+
+        if (isObject(payload)) {
+          for (const value of Object.values(payload)) {
+            const res = this.handleSubscriptDotdot(value, tree);
+            results = results.concat(res);
+          }
         }
+        if (isArray(payload)) {
+          for (const item of payload) {
+            const res = this.handleSubscriptDotdot(item, tree);
+            results = results.concat(res);
+          }
+        }
+        return results;
       }
-      return results;
     }
-  }
-};
+  };
 
-const handleSubscriptDot = (payload: unknown, tree: SubscriptDot): unknown => {
-  if (!isObject(payload)) {
-    return;
-  }
-
-  const result = handleIdentifier(payload, tree.value);
-  if (typeof result === 'undefined') {
-    return;
-  }
-
-  return handleSubscript(result, tree.next);
-};
-
-const handleSubscript = (payload: unknown, tree: Subscript): unknown => {
-  if (tree === null) {
-    return payload;
-  }
-
-  switch (tree.subtype) {
-    case 'bracket': {
-      return handleSubscriptBracket(payload, tree);
+  handleSubscriptDot = (payload: unknown, tree: SubscriptDot): unknown => {
+    if (!isObject(payload)) {
+      return;
     }
-    case 'dot': {
-      return handleSubscriptDot(payload, tree);
+
+    const result = this.handleIdentifier(payload, tree.value);
+    if (typeof result === 'undefined') {
+      return;
     }
-    case 'dotdot': {
-      return handleSubscriptDotdot(payload, tree);
+
+    return this.handleSubscript(result, tree.next);
+  };
+
+  handleSubscript = (payload: unknown, tree: Subscript): unknown => {
+    if (tree === null) {
+      return payload;
     }
-  }
-};
+
+    switch (tree.subtype) {
+      case 'bracket': {
+        return this.handleSubscriptBracket(payload, tree);
+      }
+      case 'dot': {
+        return this.handleSubscriptDot(payload, tree);
+      }
+      case 'dotdot': {
+        return this.handleSubscriptDotdot(payload, tree);
+      }
+    }
+  };
+}
 
 export const query = (payload: unknown, path: string): unknown => {
   const tree = parse(path);
 
-  return handleSubscript(payload, tree.next);
+  const handler = new Handler(payload);
+
+  return handler.handleSubscript(payload, tree.next);
 };
