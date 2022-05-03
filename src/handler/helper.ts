9b7e029c009996a31ae38fa8567a750e21bac1a6
atamano <antoine.tamano@gmail.com>
2022-05-03 17:10:13 +0200

    refactor: remove unused helper functions

--- src/handler/helper.ts
+++ src/handler/helper.ts
@@ -31,15 +31,3 @@ export const isUndefined = (item: unknown): item is undefined => {
 export const isDefined = (item: unknown): item is Exclude<typeof item, undefined> => {
   return typeof item !== 'undefined';
 };
-
-export const isArrayOfNumber = (item: unknown): item is number[] => {
-  return isArray(item) && item.every(isNumber);
-};
-
-export const standardDeviation = (arr: number[]): number => {
-  const mean = arr.reduce((acc, val) => acc + val, 0) / arr.length;
-  return Math.sqrt(
-    arr.reduce((acc, val) => acc.concat((val - mean) ** 2), [] as number[]).reduce((acc, val) => acc + val, 0) /
-      arr.length,
-  );
-};
